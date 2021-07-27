d31156884c42415d5c552120365c6a5240b40336
王戈wg <69033087+nc-animation@users.noreply.github.com>
2021-07-27 10:13:49 +0800

    Create run_test.rb

---
+++ run_test.rb
@@ -0,0 +1,14 @@
+require './archiver'
+
+# telegraph
+p fetch_article('https://telegra.ph/%E8%B4%A2%E7%BB%8F%E5%8D%81%E4%B8%80%E4%BA%BA--%E4%B8%93%E8%AE%BF%E6%96%B9%E6%96%B9%E5%A6%82%E6%9E%9C%E6%88%91%E4%B8%8D%E4%BA%A4%E4%BB%A3%E8%B0%A3%E8%A8%80%E5%B0%B1%E6%B0%B8%E8%BF%9C%E6%B2%A1%E5%AE%8C-04-18')
+
+# weixin
+p fetch_article('http://mp.weixin.qq.com/s?__biz=Mzg4OTE2MzU1Ng==&amp;mid=2247486095&amp;idx=1&amp;sn=98a427fc595b42cd804fd6ca8992f673&amp;chksm=cff150c4f886d9d2b120b42379d1e4255d2f6e9bfbac7afc7a9c3fc712540220e6484d48c286&amp;mpshare=1&amp;scene=1&amp;srcid=&amp;sharer_sharetime=1586605001116&amp;sharer_shareid=f467668849c8544e583567bf8a259f31#rd')
+p fetch_article('https://mp.weixin.qq.com/s/xDKKicV22IBRGnNnNStOVg')
+
+# weibo
+p fetch_article('https://www.weibo.com/ttarticle/p/show?id=2309404443702348087422&sudaref=weibo.com&sudaref=login.sina.com.cn&display=0&retcode=6102&sudaref=passport.weibo.com&sudaref=passport.weibo.com')
+
+# zhihu_zhuanlan
+p fetch_article('https://zhuanlan.zhihu.com/p/115765034')
